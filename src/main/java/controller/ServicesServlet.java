ba3b4492674c9f1c3f61e2d58a2a9a7ff54d1f5d
irash0005 <irash005@gmail.com>
2026-02-22 14:25:10 +0300

    создание каркасов сервлетов MastersServlet и ServicesServlet

---
+++ src/main/java/controller/ServicesServlet.java
@@ -0,0 +1,41 @@
+package  controller;
+import  jakarta.servlet.ServletException;
+import  jakarta.servlet.annotation.WebServlet;
+import  jakarta.servlet.http.HttpServlet;
+import  jakarta.servlet.http.HttpServletRequest;
+import  jakarta.servlet.http.HttpServletResponse;
+import  java.io.IOException; import  java.io.PrintWriter;
+
+/**
+* Servlet implementation class ServicesServlet
+*/
+@WebServlet("/HelloServicesServlet")
+public  class  ServicesServlet extends  HttpServlet {
+private  static  final  long  serialVersionUID  = 1L;
+/**
+* @see  HttpServlet#HttpServlet()
+*/
+public  ServicesServlet() {
+super();
+// TODO  Auto-generated constructor stub
+}
+/**
+* @see  HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+*/
+protected  void  doGet(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
+response.setContentType("text/html");
+PrintWriter writer = response.getWriter();
+try  {
+writer.println("<h2>Привет  ServicesServlet</h2>");
+} finally  {
+writer.close();  
+}
+}
+/**
+* @see  HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+*/
+protected  void  doPost(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
+// TODO  Auto-generated method stub
+doGet(request, response);
+}
+}
